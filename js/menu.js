5834814170c6ca733c12ac4f2c9c077544c80c0e
callmeish <erichendricks11@gmail.com>
2019-05-23 17:19:17 -0700

    Website framework

---
+++ js/menu.js
@@ -0,0 +1,30 @@
+$(function(){
+  var buttons = document.getElementsByTagName('button');
+  for(var i = 0, len = buttons.length; i < len; i++) {
+    buttons[i].onclick = function () {
+      changeContents(this.id);
+    }
+  }
+  
+  changeContents = function(id) {
+    selected = document.getElementsByClassName('selected');
+    for (i = 0; i < selected.length; i++) {
+      selected[i].classList.remove('selected');
+    };
+    
+    link = jQuery(document.getElementById(id));
+    link.parent().addClass('selected');
+    
+    window.location.hash = id;
+  };
+  
+  if(window.location.hash) {
+    changeContents(window.location.hash.substr(1));
+  } else {
+    window.location.hash = 'home';
+  }
+  
+  window.onhashchange = function() {
+    changeContents(window.location.hash.substr(1));
+  };
+});
