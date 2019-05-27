ddd7b357ba117d0c989e1c5a00abe6737aeb18a4
callmeish <erichendricks11@gmail.com>
2019-05-27 11:30:26 -0700

    Finished setup of races page

--- js/flexgrid.js
+++ js/flexgrid.js
@@ -1,23 +1,68 @@
 var races = ['aasimar', 'alleykin', 'arcanite', 'beastblooded', 'darkelf', 'dragonspawn', 'dwarf', 'forgeborn', 'gnome', 'halfelf', 'halforc', 'halfling', 'highelf', 'human', 'lizardfolk', 'unholyone', 'spiritborn', 'tiefling', 'woodelf'];
 
-populate = function () {
-  var container = $('.row');
-  console.log(container.id);
-
-  var gridItems = (function (id) {  
-    switch (id) {
+var populate = function() {
+  var gridItems = (function (container) {
+    if (!container) return [];
+  
+    switch (container.id) {
       case 'races':
         return races;
       default:
         return [];
     }
-  })(container.id);
+  })($('.d-flex')[0]);
 
   for (var i = 0; i < gridItems.length ; i++) {
-    var item = gridItems[i], div;
-    div = document.createElement('div');
-    div.innherHTML = gridItems[i];
-    div.classList.add('col');
-    container.appendChild(div);
+    var item = gridItems[i], div, expandButton, selectButton, expandedInfo;
+    $.getJSON('data/' + item + '.json', function (itemJSON) {
+      div = document.createElement('div');
+      div.classList.add($('.d-flex')[0].id + 'Box');
+      div.classList.add('my-2');
+      div.id = itemJSON.name.toLowerCase();
+
+      selectButton = document.createElement ('button');
+      selectButton.classList.add('p-2');
+      selectButton.classList.add('selectButton');
+      selectButton.title = itemJSON.blurb;
+      selectButton.setAttribute('data-toggle', 'tooltip');
+      selectButton.innerHTML = itemJSON.name;
+      selectButton.onclick = function () {
+        
+      };
+      div.appendChild(selectButton);
+
+      expandButton = document.createElement('button');
+      expandButton.classList.add('p-2');
+      expandButton.classList.add('expandButton');
+      expandButton.title = itemJSON.name + ' Detailed Info';
+      expandButton.setAttribute('data-toggle', 'tooltip');
+      expandButton.innerHTML = '<img src="menu.png" height="25px" width="25px"></img>';
+      expandButton.onclick = function () {
+        var flag = $(this).parent().hasClass('selectedBox')
+        $('.visible').removeClass('visible');
+        $('.selectedBox').removeClass('selectedBox');
+        if (!flag) {
+          $(this).siblings('.expInfo').addClass('visible');
+          $(this).parent().addClass('selectedBox');
+        };
+      };
+      div.appendChild(expandButton);
+
+      $(selectButton).tooltip();
+      $(expandButton).tooltip();
+
+      expandedInfo = document.createElement('div');
+      expandedInfo.classList.add('expInfo');
+      expandedInfo.innerHTML =
+        '<h4>Aasimar</h4>' +
+        '<p>Ability Score Bonus: +2 ' + itemJSON.raceBonus1 + ' or +2 ' + itemJSON.raceBonus2 + '</p>' +
+        '<p>Racial Power - ' + itemJSON.racePowerName + ': ' + itemJSON.racePowerDesc + '</p>';
+      if (itemJSON.passiveName) {
+        expandedInfo.innerHTML += '<p>Passive - ' + itemJSON.passiveName + ': ' + itemJSON.passiveDesc + '</p>';
+      }
+      div.appendChild(expandedInfo);
+
+      $('.d-flex')[0].appendChild(div);
+    });
   };
 };
--- js/menu.js
+++ js/menu.js
@@ -2,30 +2,34 @@ $(function(){
   var buttons = document.getElementsByTagName('button');
   for(var i = 0, len = buttons.length; i < len; i++) {
     buttons[i].onclick = function () {
-      changeContents(this.id);
+      menuClick(this.id);
     }
   }
   
-  changeContents = function(id) {
+  var menuClick = function(id) {
     selected = document.getElementsByClassName('selected');
     for (i = 0; i < selected.length; i++) {
       selected[i].classList.remove('selected');
     };
     
-    link = jQuery(document.getElementById(id));
+    link = $('#' + id);
     link.parent().addClass('selected');
-    
-    var module = 'modules/' + id + '.html';
-    $('title').load(module + ' title', '', function() {
-      document.title = $(this).text();
-      $('.contentPane').load(module + ' .main', populate());
-    });
 
     window.location.hash = id;
   };
+
+  var changeContents = function(id) {
+    var module = 'modules/' + id + '.html';
+    $('title').load(module + ' title', '', function() {
+      document.title = $(this).text();
+      $('.contentPane').load(module + ' .main', '', populate);
+    });
+  };
   
   if(window.location.hash) {
-    changeContents(window.location.hash.substr(1));
+    var hsh = window.location.hash;
+    menuClick(hsh.substr(1));
+    changeContents(hsh.substr(1));
   } else {
     window.location.hash = 'home';
   }
